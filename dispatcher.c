82f587decd010558e6e0f7c3ecabdda363d0d02d
Invisible-Idiot <mestrecabecao@hotmail.com>
2012-10-15 19:38:29 -0300

    preenchi a Make_context, mas tenho que revisar
    Tenho dúvidas sobre:
    -a forma de alocar a pilha;
    -a origem do uc_link;
    -o argumento void* a ser passado à função.

--- dispatcher.c
+++ dispatcher.c
@@ -1,13 +1,14 @@
 #include "TCB.h"
+#define STACKSIZE 10485760
 
-ucontext_t* Make_context(void (*start_routine) (void*), void* arg)
+ucontext_t* Make_context(void (*start_routine) (void*), void* arg, ucontext_t* uclink)
 {
 	ucontext_t* context;
 	
 	getcontext(context);
-	context.uc_stack.ss_sp = ??;
-	context.uc_stack.ss_size = ??;
-	context.uc_link = ??;
+	context.uc_stack.ss_sp = mmap(NULL,STACKSIZE,PROT_WRITE|PROT_READ,MAP_PRIVATE|MAP_GROWSDOWN|MAP_ANONYMOUS,-1,0);
+	context.uc_stack.ss_size = STACKSIZE;
+	context.uc_link = uclink;
 	makecontext(context, start_routine, 1, arg);
 
 	return context;
