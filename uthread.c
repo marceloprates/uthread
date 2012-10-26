f19f96db0aefb4b94b93bcb87280720f04386dec
Invisible-Idiot <mestrecabecao@hotmail.com>
2012-10-25 23:12:05 -0200

    implementei uthread_init
    a função trata a main que a chamou como uma thread também, salvando seu contexto e a inserindo na fila; para que as outras threads continuem rodando quando a main encerra, foi necessário setar o uc_link do seu contexto, e por causa disso on_exit virou uma variável global

--- uthread.c
+++ uthread.c
@@ -2,6 +2,8 @@
 #include "dispatcher.h"
 #include "contexts.h"
 
+ucontext_t* on_exit;
+
 int Dispatch_next_thread()
 {
 	TCB* thread = Schedule();
@@ -25,25 +27,46 @@ void Change_current_thread()
 void Exit_thread()
 {
 	TCB* thread = Running();
-	free(thread); // Frees pointer to thread that exited.
+	free(thread); // Frees pointer to thread that exited
 	Dispatch_next_thread();
 }
 
 //
 
+int uthread_init()
+{
+	ucontext_t* main_context = (ucontext_t*)malloc(sizeof(ucontext_t));
+	bool gotcontext = 0;
+
+	on_exit = Make_context_noargs(Exit_thread, NULL); // Context to be entered when the thread exits
+
+	getcontext(main_context);
+
+	if(!gotcontext)
+	{
+		gotcontext = 1;
+
+		// Sets the context to run when the main thread exits, so that other threads can keep running
+		main_context->uc_link = on_exit;
+
+		Create(main_context); // Creates main thread
+
+		// Sets main thread to run; this is necessary so the running context is the one with its uc_link set to on_exit
+		Dispatch_next_thread(); 
+	}
+
+	returns something; // TODO: test for errors
+}
+
 int uthread_create(void * (*start_routine)(void*), void * arg)
 {
-	ucontext_t* on_exit;
-	Ucontext_t* thread_context;
+	ucontext_t* thread_context;
 
-	on_exit = Make_context_noargs(Exit_thread, NULL); // Context to be entered when the thread exits.
+	thread_context = Make_context(start_routine, arg, on_exit); // Create thread context that runs start_routine
 
-	thread_context = Make_context(start_routine, arg, on_exit); // Create thread context that runs start_routine.
+	Create(thread_context); // Creates thread and inserts it on queue
 
-	Create(thread_context); // Creates thread and inserts it on queue.
-	Dispatch_next_thread(); // Schedules and dispatches next thread.
-
-	returns something; // TODO: test for errors.
+	returns something; // TODO: test for errors
 }
 
 void uthread_yield()
@@ -52,10 +75,10 @@ void uthread_yield()
 
 	Save(Running()); // Saves 'here' as current thread context.
 
-	if(!has_yielded) // Stops thread only if thread has not yielded yet.
+	if(!has_yielded) // Stop thread only if thread has not yielded yet.
 	{
 		has_yielded = 1;
-		change_current_thread();
+		Change_current_thread();
 	}
 	else // If thread has yielded, we're returning to this point via context switching: we don't stop the thread, just return.
 	{
@@ -67,13 +90,13 @@ int uthread_join(TCB* waited_for)
 {
 	TCB* this_thread = Running();
 	
-	Block(this_thread, waited_for); // Blocks this thread's TCB.
+	Block(this_thread, waited_for); // Blocks this thread's TCB
 
 	Save(this_thread); // Saves 'here' as current thread context.
 
-	if(Is_Blocked(this_thread)) // Stops thread only if thread is blocked.
+	if(Is_Blocked(this_thread)) // Stop thread only if thread is blocked.
 	{
-		change_current_thread();
+		Change_current_thread();
 	}
 	else // If thread isn't blocked, we're returning to this point via context switching: we don't stop the thread, just return.
 	{
