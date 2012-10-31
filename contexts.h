b8b58d784f4d50d96680a6762644ba8838c69906
Invisible-Idiot <mestrecabecao@hotmail.com>
2012-10-31 19:51:22 -0200

    Movi includes para o .c

--- contexts.h
+++ contexts.h
@@ -1,6 +1,4 @@
 #include <ucontext.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 ucontext_t* Make_context_noargs(void (*start_routine) (void), ucontext_t* uclink);
 ucontext_t* Make_context(void (*start_routine) (void*), void* arg, ucontext_t* uclink);
